a6a72071b816e0451820c22faec1ff4955312a27
Kornelius Septa Jasa Sipayung <writecodex09@gmail.com>
2021-11-02 20:11:48 +0700

    implement fungsi penjumlahan

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/writecodex09/synacedemy-app
+
+go 1.14
+
+require github.com/writecodex09/synacedemy-module v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	synacedemy_module "github.com/writecodex09/synacedemy-module"
+)
+
+func main() {
+	fmt.Println("Implementasi fungsi penjumlahan")
+	synacedemy_module.Penjumlahan(2, 4)
+}
+
